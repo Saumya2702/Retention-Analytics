e39b8f6d9242ab163ab6072877e44fbbb2b1f27c
Saumya Anand <132563927+Saumya2702@users.noreply.github.com>
2025-09-01 02:28:19 +0530

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,13 @@
-<<<<<<< HEAD
+
 export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 }
-=======
 export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 }
->>>>>>> c202deee64a173be04b38465b789d776181c2fe9
